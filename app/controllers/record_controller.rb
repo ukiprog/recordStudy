97398e570beb73289f0c9dd6ba83531c459f1cd5
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-09-22 22:26:27 +0900

    勉強時間合計算出

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,10 +1,10 @@
 class RecordController < ApplicationController
 
   def index # 今週の学習記録
-    getStartWeek(Date.today)
-    getWeekRecords(@startWeek)
-    @record = Record.new
+    @startWeek = getStartWeek(Date.today)
     @subject = Subject.all
+    @records = getWeekRecords(@startWeek, @subject)
+    @record = Record.new
   end
 
   def create
@@ -18,22 +18,39 @@ class RecordController < ApplicationController
   #------------------------------------------------
   private
   def getStartWeek(date)
-    @startWeek = date - (date.wday == 0 ? 6 : (date.wday - 1))
+    startWeek = date - (date.wday == 0 ? 6 : (date.wday - 1))
+    return startWeek
   end
 
-  def getWeekRecords(from)
-    records = current_user.records.where("start >= ? AND start < ?", from, from + 7).order(start: :asc, end: :asc)
-
-    @records = {}
+  def getWeekRecords(from, subject)
+    data = current_user.records.where("start >= ? AND start < ?", from, from + 7).order(start: :asc, end: :asc)
+    records = {}
     for i in 0..6
       date = from + i
-      @records[date] = []
-      records.each do |record|
+      records[date] = []
+      data.each do |record|
         if record.start >= date and record.start < date + 1
-          @records[date].push(record)
+          records[date].push(record)
+        end
+      end
+      records[date].push({ summary: getSummary(records[date], subject) })
+    end
+
+    return records
+  end
+
+  def getSummary(record, subjects)
+    summary = {}
+    subjects.each do |subject|
+      summary[subject.id] = 0
+      record.each do |f|
+        if f.subject_id == subject.id
+          summary[subject.id] += f.end - f.start
         end
       end
     end
+
+    return summary
   end
 
   def createRecordParams
