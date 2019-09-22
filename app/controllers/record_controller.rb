3fb001c2c3751a15afb9e869729638fbd4109399
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-09-22 17:00:45 +0900

    今週の学習記録データの取得

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,6 +1,29 @@
 class RecordController < ApplicationController
+
   def index # 今週の学習記録
-    @today = Date.today
-    @startThisWeek = @today - (@today.wday == 0 ? 7 : (@today.wday - 1))
+    getStartThisWeek
+    getThisWeekRecords
+  end
+
+  #------------------------------------------------
+  private
+  def getStartThisWeek
+    today = Date.today
+    @startThisWeek = today - (today.wday == 0 ? 6 : (today.wday - 1))
+  end
+
+  def getThisWeekRecords
+    records = current_user.records.where("start >= ? AND end < ?", @startThisWeek, @startThisWeek + 7).order(start: :asc, end: :asc)
+
+    @records = {}
+    for i in 0..6
+      date = @startThisWeek + i
+      @records[date] = []
+      records.each do |record|
+        if record.start >= date and record.start < date + 1 then
+          @records[date].push(record)
+        end
+      end
+    end
   end
 end
