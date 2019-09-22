555107b37bb7bb65b7e447f8c20eca139a51afc6
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-09-22 15:05:17 +0900

    今週の学習記録 マークアップ（ほぼ完成）

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,4 +1,6 @@
 class RecordController < ApplicationController
-  def index
+  def index # 今週の学習記録
+    @today = Date.today
+    @startThisWeek = @today - (@today.wday == 0 ? 7 : (@today.wday - 1))
   end
 end
