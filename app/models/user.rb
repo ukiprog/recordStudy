d5dde1dc13959efaa4b4bc30f82c1e1793fdc338
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-09-24 01:12:39 +0900

    comment表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :records
+  has_many :comments
 
   validates :name, presence: true
   validates :position, presence: true
