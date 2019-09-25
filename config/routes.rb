02f11ecb0e3ffa5b758fcb2c8dc5cba50f891530
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-09-25 22:38:28 +0900

    recordからattachを独立

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'record#show'
   resources :record, only: [:index, :show, :create, :edit, :update, :destroy]
   resources :comment, only: [:create, :update]
+  resources :attach, only: [:create]
   devise_for :users
 end
