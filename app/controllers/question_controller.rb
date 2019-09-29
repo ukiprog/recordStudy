842a33025104972f0b9b60d7993db182332e6cde
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-09-29 20:46:11 +0900

    質問作成サーバーサイド実装

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,9 +1,20 @@
 class QuestionController < ApplicationController
   def index
     @questions = current_user.questions
+    @question = Question.new
+    @subjects = Subject.all
   end
 
   def create
+    if Question.create(question_params)
+      redirect_to question_index_path
+    else
+      render :index
+    end
+  end
 
+  private
+  def question_params
+    params.require(:question).permit(:user_id, :subject_id, :question)
   end
 end
