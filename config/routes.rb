8d0a4aac04726c8876945bcd66fca72fa7222f07
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-10-22 18:00:34 +0900

    一覧用ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'application#top'
   resources :record, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :teacher_record, only: [:index]
   resources :comment, only: [:create, :update]
   resources :attach, only: [:create]
   resources :question, only: [:index, :create, :destroy]
