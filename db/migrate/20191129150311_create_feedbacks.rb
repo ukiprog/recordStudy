2b3111fdc3b8c723af8ed5d2f0f11b383c784753
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-11-30 00:23:53 +0900

    feedback.student_id データ型修正

--- db/migrate/20191129150311_create_feedbacks.rb
+++ db/migrate/20191129150311_create_feedbacks.rb
@@ -3,7 +3,7 @@ class CreateFeedbacks < ActiveRecord::Migration[5.2]
     create_table :feedbacks do |t|
       t.references :user
       t.string :comment
-      t.string :student_jd
+      t.bigint :student_jd
       t.datetime :comment_at, null: false
       t.timestamps
     end
